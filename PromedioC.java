b1b705bb425a2375e244038260ec2ece72e7c8ec
icarreons <akatsuki96_ivan@hotmail.com>
2017-08-19 23:32:22 -0500

    Todos necesitamos un cambio

--- PromedioC.java
+++ PromedioC.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trabajos;
 
-/**
- *
- * @author jesuseduardo
- */
 public class PromedioC {
     public static void main(String[] args) 
     {
@@ -16,7 +7,9 @@ public class PromedioC {
         c1 = Double.parseDouble(javax.swing.JOptionPane.showInputDialog(null,"Primer Calificacion"));
         c2 = Double.parseDouble(javax.swing.JOptionPane.showInputDialog(null,"Segunda Calificacion"));
         c3 = Double.parseDouble(javax.swing.JOptionPane.showInputDialog(null,"Tercera Calificacion"));
-        javax.swing.JOptionPane.showMessageDialog(null,"Calificacion 1: " + c1 + "\nCalificacion 2: " + c2 + "\nCalificacion 3: " + c3 + "\nPromedio: " + ((c1 + c2 + c3)/3));
+	suma=(c1+c2+c3);
+	promedio=(suma/3);
+        javax.swing.JOptionPane.showMessageDialog(null,"Calificacion 1: " + c1 + "\nCalificacion 2: " + c2 + "\nCalificacion 3: " + c3 + "\nPromedio: " + promedio);
     }
     
 }
